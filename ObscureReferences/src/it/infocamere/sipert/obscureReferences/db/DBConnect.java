ec894e3a3ef1474b758fd8e3c10da031beb609eb
YYI4627 <YYI4627@ICVDISVILV6-008.ic.intra.infocamere.it>
2020-02-18 16:43:33 +0100

    altro aggiustamento per la gestione delle connessioni

--- ObscureReferences/src/it/infocamere/sipert/obscureReferences/db/DBConnect.java
+++ ObscureReferences/src/it/infocamere/sipert/obscureReferences/db/DBConnect.java
@@ -1,6 +1,8 @@
 package it.infocamere.sipert.obscureReferences.db;
 
 import java.io.File;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -17,6 +19,8 @@ import it.infocamere.sipert.obscureReferences.util.TnsOraParser;
 public class DBConnect {
 	
 	static private final String jdbcUrlFirstBlock = "jdbc:oracle:thin:@//";
+	
+	static private final String jdbcUrlFirstBlockForTNS = "jdbc:oracle:thin:@";
 	
 	static private final String HOST = "HOST";
 	static private final String PORT = "PORT";
@@ -66,23 +70,19 @@ public class DBConnect {
 		String port = tnsOra.getValueFromNameParameter(PORT, parameters);		
 		String serviceNane = tnsOra.getValueFromNameParameter(SERVICE_NAME, parameters);
 		
-		if (host == null || (host != null && host.length() < 1)) {
-    		log.error("Impossibile accedere al data base: host data base non trovato");
-    		return null;			
+		if (host == null || (host != null && host.length() < 1) || (host != null && host.length() > 0 && hostIsKO(host))) {
+    		log.warn("Impossibile accedere al data base tramite l'host name -> si procede tramite l'id connessione/tns");
+    		return getConnectionByTNS(username, password, tns_admin, schema.getIdConnessione(), serviceNane, schema.getSchemaUserName());
 		}
-		
 		if (port == null || (port != null && port.length() < 1)) {
     		log.error("Impossibile accedere al data base: porta data base non trovato");
     		return null;			
 		}		
-		
 		if (serviceNane == null || (serviceNane != null && serviceNane.length() < 1)) {
     		log.error("Impossibile accedere al data base: servece name data base non trovato");
     		return null;			
 		}				
-		
 		String url = jdbcUrlFirstBlock + host + ":" + port + "/" + serviceNane;
-	    
         try {
     	    
         	return DriverManager.getConnection(url, username, password);
@@ -94,5 +94,43 @@ public class DBConnect {
 
 		
 	}	
+	
+	private static boolean hostIsKO(String host) {
+		
+		boolean isKO = true;
+		
+        InetAddress domainInetAddress = null;
+        
+        try {
+            domainInetAddress = InetAddress.getByName(host);
+            isKO = false;
+        } catch (UnknownHostException uhe) {
+        	return isKO;
+        }
+		
+		return isKO;
+	}
+	
+	private static Connection getConnectionByTNS(String username, String password, String tns_admin, String idConnessione, String serviceNane, String schemaUserName) {
+		
+		String url = jdbcUrlFirstBlockForTNS + idConnessione;
+		
+		System.setProperty("oracle.net.tns_admin", tns_admin);
+		
+        try {
+        	
+        	Class.forName ("oracle.jdbc.OracleDriver");
+    	    
+        	return DriverManager.getConnection(url, username, password);
+        	
+        } catch (SQLException e) {
+			//e.printStackTrace();
+			throw new RuntimeException("SCHEMA " + schemaUserName + " " + e.toString());
+        } catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		return null;
+	}
 	
 }
